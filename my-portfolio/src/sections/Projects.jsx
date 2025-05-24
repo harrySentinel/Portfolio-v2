19ebf3e2d95d06d62bb9ac783f4a03e55c4be4fb
Aditya Srivastava <adityasri1801@gmail.com>
2025-05-24 23:56:59 +0530

    starting Project section

---
+++ my-portfolio/src/sections/Projects.jsx
@@ -0,0 +1,28 @@
+
+
+const portfolioProjects = [
+    {
+        company: "Acme Corp",
+        year: "2022",
+        title: "Dark Saas Landing Page",
+        results: [
+            {title: "Enhanced user experience by 40%" },
+            {title: "Improved site speed by 50%" },
+            {title: "Increased mobile traffic by 35%" },
+        ],
+        link: "https://youtu.be/_CvliDAXSCg?si=z0XDYc4_U2LOlErs",
+        image: darkSaasLandingPage,
+    },
+    {
+        company: "Acme Corp",
+        year: "2022",
+        title: "Dark Saas Landing Page",
+        results: [
+            {title: "Enhanced user experience by 40%" },
+            {title: "Improved site speed by 50%" },
+            {title: "Increased mobile traffic by 35%" },
+        ],
+        link: "https://youtu.be/_CvliDAXSCg?si=z0XDYc4_U2LOlErs",
+        image: darkSaasLandingPage,
+    }
+]
