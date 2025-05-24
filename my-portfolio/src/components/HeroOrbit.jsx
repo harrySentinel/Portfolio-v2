5248d756d2e0c62c10382484c4b979a0cda31095
Aditya Srivastava <adityasri1801@gmail.com>
2025-05-24 11:48:01 +0530

    hero section done, animation to add

--- my-portfolio/src/components/HeroOrbit.jsx
+++ my-portfolio/src/components/HeroOrbit.jsx
@@ -2,7 +2,7 @@ export const HeroOrbit = ({ children, size, rotation }) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className=""
+        className="flex items-start justify-start"
         style={{
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
