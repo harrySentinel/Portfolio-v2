6f306482508ee14b52267bb0365d320eb9c78a5a
Aditya Srivastava <adityasri1801@gmail.com>
2025-05-29 02:12:49 +0530

    tape animation completed

--- my-portfolio/tailwind.config.mjs
+++ my-portfolio/tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { transform } from 'next/dist/build/swc/generated-native';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -25,16 +27,25 @@ export default {
           serif: ['var(--font-serif)', 'serif'],
         },
         animation: {
-           'ping-large': "ping-large 1s ease-in-out infinite"
+           'ping-large': "ping-large 1s ease-in-out infinite",
+           'move-left': 'move-left 1s linear infinite'
         },
         keyframes: {
           'ping-large': {
             '75%, 100%': {
               transform: 'scale(3)',
               opacity: '0',
+            },
+          },
+          'move-left': {
+            '0%': {
+              transform: 'translateX(0%)'
+            },
+            '100%': {
+              transform: 'translateX(-50%)'
             }
           }
-        }
+        },
       },
   },
   plugins: [],
